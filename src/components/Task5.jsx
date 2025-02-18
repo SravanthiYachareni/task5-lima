9988f1dd6ca48578791172ad09b20d6057a2377b
SravanthiYachareni <sravanthiyachareni@gmail.com>
2025-02-18 16:57:52 +0530

    added

--- src/components/Task5.jsx
+++ src/components/Task5.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './task5.css'
 import { MdModeEdit } from "react-icons/md";
+import { FaArrowDown } from "react-icons/fa";
+import { FaSort } from "react-icons/fa6";
 
 function Task5() {
     const [list, setList] = useState([])
@@ -8,6 +10,7 @@ function Task5() {
     const [addExperience, setExperiecne] = useState([])
     const [selected, setSelected] = useState(false)
     const [isVisisble, setIsvisisble] = useState(false)
+    
     const [employee, setEmployee] = useState({
         id: Date.now(),
         empId: '',
@@ -61,6 +64,78 @@ function Task5() {
         setEditId(row.id)
     }
     console.log(addExperience)
+   
+    const [sorted,setSorted]  = useState( { sorted:'empId', reversed:false})
+    const sortById = () => {
+        setSorted({sorted:'empId',reversed: !sorted.reversed})
+        const userCopy = [...list]
+        userCopy.sort((userA,userB)=> {
+            if(sorted.reversed) {
+                return userA.empId - userB.empId;
+            }
+            return userB.empId - userA.empId;
+        })
+        setList(userCopy)
+    }
+
+    const sortByName = () => {
+        setSorted({sorted:'firstname',reversed: !sorted.reversed})
+        const userCopy = [...list];
+        userCopy.sort((userA,userB) => {
+            const nameA = `${userA.firstname}`
+            const nameB = `${userB.firstname}`
+            if(sorted.reversed) {
+                return nameB.localeCompare(nameA)
+
+            }
+            return nameA.localeCompare(nameB)
+        })
+        setList(userCopy)
+        
+    }
+
+    const sortByName1 = () => {
+        setSorted({sorted:'lastname',reversed: !sorted.reversed})
+        const userCopy = [...list];
+        userCopy.sort((userA,userB) => {
+            const nameA = `${userA.lastname}`
+            const nameB = `${userB.lastname}`
+            if(sorted.reversed) {
+                return nameB.localeCompare(nameA)
+
+            }
+            return nameA.localeCompare(nameB)
+        })
+        setList(userCopy)
+        
+    }
+    const [search,setSearch] =useState('')
+    console.log(search)
+
+    const emp = list.filter((item)=>{
+        return (
+    item.empId.toString().toLowerCase().includes(search) 
+    // item.firstname.toString().toLowerCase().includes(search) || 
+    // item.lastname.toString().toLowerCase().includes(search) || 
+    // item.email.toString().toLowerCase().includes(search)  
+    // item.gender.toString().toLowerCase().includes(search) || 
+    // item.role.toString().toLowerCase().includes(search)  
+    )})
+
+    //Pagination
+    // const [currentPage,setCurrent] = useState(1);
+    // const recordPerPage = 5;
+    // const  lastIndex = currentPage * recordPerPage; 
+    // const firstIndex = lastIndex - recordPerPage;
+    // const records = employee.slice(firstIndex,lastIndex);
+    // const npage = Math.ceil(employee.length/recordPerPage);
+    // const numbers = [...list(npage + 1).key()].slice(1);
+
+    // const prePage = () => {
+
+    // }
+
+
     return (
 
         <div>
@@ -108,7 +183,7 @@ function Task5() {
 
                                                     <div className='form-group5'>
                                                         {/* <label>Gender</label> */}
-                                                        <select name='gender' onChange={handleChange}>
+                                                        <select name='gender' value='' onChange={handleChange} >
                                                             <option > Male</option>
                                                             <option >Female</option>
                                                             <option >Others</option>
@@ -145,11 +220,11 @@ function Task5() {
                                                     addExperience.map((add, i) => {
                                                         return (
                                                             <div className='container-box'>
-                                                                 <button id='delete-btn'>Delete</button>
+
                                                                 <div className='experience'>
-                                                               
+
                                                                     <div className='company'>
-                                                                        
+
                                                                         <input type='text' placeholder='Company' name='company'
                                                                             value={add.company} onChange={(e) => { handleExperienceChange(i, e) }} />
 
@@ -192,33 +267,66 @@ function Task5() {
 
 
                 <div >
+                    {/* <div>
+                        <input className='search' type='text'
+                            placeholder='Search'
+                            onChange={(e) => setSearch(e.target.value)} />
+                    </div> */}
+                    <div className='search'>
+                        <input type='text' placeholder='search by ID'
+                        onChange={(e)=>setSearch(e.target.value)}/>
+                    </div>
                     <table id='emp-table'>
                         <tr>
-                            <th>Emp Id</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
+                            <th >Emp Id<span onClick={sortById} style={{paddingLeft:'6px',paddingTop:'4px',opacity:'0.6'}}><FaSort /></span></th>
+                            <th >First Name<span onClick={sortByName} style={{paddingLeft:'6px',paddingTop:'4px',opacity:'0.6'}}><FaSort /></span></th>
+                            <th >Last Name <span onClick={sortByName1} style={{paddingLeft:'6px',paddingTop:'4px',opacity: '0.6'}}><FaSort/></span></th>
                             <th>Email</th>
                             <th>Gender</th>
                             <th>Role</th>
                             <th>Action</th></tr>
                         <tbody>
-                            {list.map((item, i) => {
-                                return (
-                                    <tr>
-                                        <td>{item.empId}</td>
-                                        <td>{item.firstname}</td>
-                                        <td>{item.lastname}</td>
-                                        <td>{item.email}</td>
-                                        <td>{item.gender}</td>
-                                        <td>{item.role}</td>
-                                        <td ><button id='edit' onClick={() => { handleEdit(item) }}>edit<MdModeEdit style={{ width: '30%' }} />
-                                        </button>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
+                            <tr>
+                                {/* <td><input type='text' value={search.empId} onChange={(e) => setSearch(e.target.value)} /></td>
+                                <td><input type='text' value={search.firstname} onChange={(e) => setSearch(e.target.value)} /></td>
+                                <td><input type='text' value={search.lastname} onChange={(e) => setSearch(e.target.value)} /></td>
+                                <td><input type='text' value={search.email} onChange={(e) => setSearch(e.target.value)} /></td>
+
+                                <td><input type='text' value={search.gender} onChange={(e) => setSearch(e.target.value)} /></td> */}
+                            </tr>
+                            {emp
+                                .map((item, i) => {
+                                    return (
+                                        <tr>
+                                            <td>{item.empId}</td>
+                                            <td>{item.firstname}</td>
+                                            <td>{item.lastname}</td>
+                                            <td>{item.email}</td>
+                                            <td>{item.gender}</td>
+                                            <td>{item.role}</td>
+                                            <td ><button id='edit' onClick={() => { handleEdit(item) }}>edit<MdModeEdit style={{ width: '30%' }} />
+                                            </button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
                         </tbody>
                     </table>
+                    {/* <nav>
+                        <ul className='pagination'> 
+                            <li className='page-item'>
+                                <a href='#' className='page-link' onClick={prePage}>Prev</a>
+
+                            </li>
+                            {
+                                numbers.map((n,i)=>(
+                                    <li>
+                                            <a>{n}</a>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </nav> */}
                 </div>
             </div>
         </div>
